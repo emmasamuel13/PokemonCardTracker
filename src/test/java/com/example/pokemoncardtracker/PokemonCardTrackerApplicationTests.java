2bdd797eef63e80cd88d486d171cd56c7a57f8d2
Emma E Samuel <emma.e.samuel@uk.ey.com>
2023-09-12 11:53:34 +0100

    It doesn't work but I need to save

---
+++ src/test/java/com/example/pokemoncardtracker/PokemonCardTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pokemoncardtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonCardTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
