cd78819adf4d14e549ea022e2eea140c556e061c
Emma E Samuel <emma.e.samuel@uk.ey.com>
2023-10-24 14:40:08 +0100

    Setup and chapter 1 Tour of Heroes tutorial

---
+++ src/app/pokemon/pokemon-card.ts
@@ -0,0 +1,10 @@
+export interface PokemonCard {
+    id: number;
+    pokemonName: string;
+    cardLanguage: string;
+    setName: string;
+    setReleaseDate: string;
+    cardNumber: number;
+    cardsInSet: number;
+    inCollection: boolean;
+}
